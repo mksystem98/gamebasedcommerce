4a74da60d747eb0b7bcb5159cbef109d362d1682
mksystem98 <mksystem98@gmail.com>
2023-12-22 15:04:14 +0900

    first commit

---
+++ src/main/java/mk/gamebasedcommerce/GamebasedcommerceApplication.java
@@ -0,0 +1,13 @@
+package mk.gamebasedcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebasedcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamebasedcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/gamebasedcommerce/GamebasedcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.gamebasedcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamebasedcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
